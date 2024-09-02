821a7cf8b142f6faf7181b56a4a80c9405d357ca
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-02 10:25:59 -0700

    Add router, static resource, notes endpoint handler

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const router = require('./scripts/routes/router');
+const path = require('path');
 
-app.use(express.static('public'));
+app.use('/api', router);
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/'))
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Listening at port https://localhost:${PORT}`)
