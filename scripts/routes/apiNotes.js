411b94b6247ef5800ce461350a02dc9cd31f1fa3
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-02 10:24:53 -0700

    Update notes router name, add POST handler

---
+++ scripts/routes/apiNotes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const apiNotesRouter = express.Router();
+const { readFile, appendToDatabase } = require('../utility/routerUtil');
+
+// Add json parser middleware
+apiNotesRouter.use(express.json())
+
+// Add GET endpoint handling
+apiNotesRouter.get('/', (req, res) => {
+  const apiNotesRouterData = readFile('/db/db.json', true);
+  res.send(apiNotesRouterData);
+})
+
+// Add POST endpoint handling
+apiNotesRouter.post('/', (req, res) => {
+  console.log('in post')
+  console.log(req.body);
+  const { title, text } = req.body;
+
+  if(title && text){
+    appendToDatabase({ title: title, text: text});
+  }
+
+  res.status(200).send();
+  // title: noteTitle.value,
+  // text: noteText.value
+})
+
+module.exports = apiNotesRouter;
