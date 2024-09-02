df8956f91cfbf2b9d3aae90308d03ece4a2604e9
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-02 11:08:27 -0700

    Remove debug logs, add endpoint call logs

--- scripts/routes/apiNotes.js
+++ scripts/routes/apiNotes.js
@@ -8,27 +8,26 @@ apiNotesRouter.use(express.json())
 
 // Add GET endpoint handling
 apiNotesRouter.get('/', (req, res) => {
+  console.log('/api/notes GET call received');
   const apiNotesRouterData = readFile('/db/db.json', true);
   res.send(apiNotesRouterData);
 })
 
 // Add POST endpoint handling
 apiNotesRouter.post('/', (req, res) => {
-  console.log('in post')
-  console.log(req.body);
+  console.log('/api/notes POST call received');
   const { title, text } = req.body;
-
+  
   if(title && text){
     appendToDatabase({ title: title, text: text, id: uuidv4()});
   }
-
+  
   res.status(200).send();
-  // title: noteTitle.value,
-  // text: noteText.value
 })
 
 // Add DELETE handling
 apiNotesRouter.delete('/:id', (req, res) => {
+  console.log('/api/notes DELETE call received');
     const noteId = req.params.id;
     deleteNoteFromDatabase(noteId);
     res.status(200).send();
